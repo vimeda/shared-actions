d11b6623823eac5700db96519b69380a2afd0f71
libracoder <libracoder@gmail.com>
2023-07-24 11:25:52 +0100

    add lambda_patching scripts

--- .github/workflows/scripts/get_env_values.py
+++ .github/workflows/scripts/get_env_values.py
@@ -1,34 +1,49 @@
-#!/usr/bin/env python3
-
-import yaml
 import subprocess
+import json
 
-# Fetch parameter values from AWS Systems Manager Parameter Store
-parameter_values = {}
-with open("../envs", "r") as env_file:
-    for line in env_file:
-        parameter = line.strip()
-        value = subprocess.check_output(
-            ["aws-vault", "exec", "precious.okwu", "--", "aws", "ssm", "get-parameter", "--name", parameter, "--query", "Parameter.Value", "--output", "text"],
-            universal_newlines=True
-        ).strip()
-        parameter_values[parameter] = value
+def run_op_command(command):
+    try:
+        result = subprocess.run(command, shell=True, capture_output=True, text=True, check=True)
+        return result.stdout.strip()
+    except subprocess.CalledProcessError as e:
+        print(f"Error executing the command: {e}")
+        return None
+    except Exception as ex:
+        print(f"An unexpected error occurred: {ex}")
+        return None
 
-# Specify the output file path
-output_file_path = "./envs.out"
+def save_to_env(labels_values, output_file):
+    with open(output_file, "w") as env_file:
+        for label, value in labels_values.items():
+            env_file.write(f"{label}: {value}\n")
 
-# Write the parameter-value pairs as plain text to the new file "envs.out" (overwriting if it exists)
-with open(output_file_path, "w") as file:
-    for param, value in parameter_values.items():
-        file.write(f"{param}: {value}\n")
+def main():
+    # Execute the initial op command to get the sample data
+    initial_command = 'op items get order-srv --format=json'
+    sample_data = run_op_command(initial_command)
 
-# Store the value in a YAML file
-yaml_data = {
-    "envVariables": [
-        {"variables": parameter_values}
-    ]
-}
+    if sample_data is None:
+        print("Error fetching sample data.")
+        return
 
-# Write the YAML data to the "envs.out" file in YAML format
-with open(output_file_path, "w") as yaml_file:
-    yaml.dump(yaml_data, yaml_file, default_flow_style=False)
+    # Parse the JSON data
+    parsed_data = json.loads(sample_data)
+
+    labels_values = {}
+
+    # Extract the label and execute the command for each label
+    for field in parsed_data['fields']:
+        label = field['label']
+        value = run_op_command(f'op read op://errsir3kqd4gdjgaxliofyskey/order-srv/{label}')
+        if value is not None:
+            labels_values[label] = value
+
+    # Set the output file name
+    output_file = "./envs.out"
+
+    # Save the values to the output file
+    save_to_env(labels_values, output_file)
+    print("Values saved to .env file.")
+
+if __name__ == "__main__":
+    main()
