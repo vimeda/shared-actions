642ca25dccf79a4140ccc2fad49539701d4276df
Precious Okwu <32944039+libracoder@users.noreply.github.com>
2024-10-15 13:37:41 +0100

    update terraform config

--- crossplane/main.tf
+++ crossplane/main.tf
@@ -40,6 +40,6 @@ data "kubectl_file_documents" "claims" {
 }
 
 resource "kubectl_manifest" "claim" {
-  count     = length(data.kubectl_file_documents.claims.documents)
-  yaml_body = element(data.kubectl_file_documents.claims.documents, count.index)
+  for_each  = data.kubectl_file_documents.claims.manifests
+  yaml_body = each.value
 }
