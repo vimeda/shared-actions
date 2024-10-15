7e10205927bdff9f215fe02b070e27da45d7f46b
Kieran Patel <me@kieranajp.co.uk>
2024-10-15 13:01:14 +0200

    Try creating kubeconfig

--- crossplane/versions.tf
+++ crossplane/versions.tf
@@ -22,17 +22,8 @@ terraform {
   }
 }
 
-data "aws_eks_cluster" "cluster" {
-  name = var.cluster_name
-}
-
-data "aws_eks_cluster_auth" "cluster" {
-  name = var.cluster_name
-}
-
 provider "kubectl" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-  load_config_file       = false
+  config_path       = "/home/runner/config"
+  load_config_file  = true
+  apply_retry_count = 15
 }
