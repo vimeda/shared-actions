b4038f34190dbc5e45a970eab8491bb13768532f
Precious Okwu <32944039+libracoder@users.noreply.github.com>
2024-10-17 17:18:36 +0100

    use output of yamls files instead temp file

--- crossplane/main.tf
+++ crossplane/main.tf
@@ -33,8 +33,8 @@ locals {
 # Parse the YAML content into Kubernetes documents using kubectl provider
 data "kubectl_file_documents" "claims" {
   depends_on = [data.external.modified_yaml]  # Ensure this runs after the external data source
-  for_each   = local.yaml_files
-  content    = file("${local.yaml_dir}/${each.value}")  # Read the file content for each YAML file
+  for_each = data.external.modified_yaml
+  content  = each.value.result.modified_claims_yaml
 }
 
 output "kubectl_manifest" {
