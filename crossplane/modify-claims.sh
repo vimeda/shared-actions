c395df35681e06693044f443e7ed78efb720d07f
Precious Okwu <32944039+libracoder@users.noreply.github.com>
2025-02-04 14:55:04 +0100

    remove XLykonS3EventSourceMapping

--- crossplane/modify-claims.sh
+++ crossplane/modify-claims.sh
@@ -75,7 +75,7 @@ elif [[ " ${CLAIM_TYPES_GOAPP[@]} " =~ " ${kind} " ]]; then
   yq eval ".spec.parameters.vault_id = \"$vault_id\"" -i "$temp_yaml_file"
 fi
 
-if [[ "$kind" == "XLykonLambdaDockerImage" || "$kind" == "XEventSourceMapping"|| "$kind" == "XLykonS3EventSourceMapping" ]]; then
+if [[ "$kind" == "XLykonLambdaDockerImage" || "$kind" == "XEventSourceMapping" ]]; then
   # Get the current stream value
   stream=$(yq eval '.spec.parameters.stream' "$temp_yaml_file")
 
